e2a6963112e95e9b64e1dbfafa6c6d8c6f83117d
Nathanael Wettstein <nathanael.wettstein@gmail.com>
2019-04-06 01:11:56 +0200

    Get logo text from GET params.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/logo'))
+  .get('/:text?', (req, res) => res.render('pages/logo', { text: req.params.text }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
